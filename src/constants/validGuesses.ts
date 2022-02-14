a372852bdc0fa58a1c85cebe91fb8cb93b95df7f
gushint <gushintermeister@gmail.com>
2022-02-14 17:46:16 -0600

    edit word list

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -1,4 +1,10 @@
 export const VALID_GUESSES = [
-  'state',
-  'texas',
+  'state', // States In america
+  'texas', // Used to live in texas
+  'potus', // president of the united states
+  'money', // $
+  'tests', // tests in school
+  'class', // classes in school
+  'study', // studying in school 
+  'learn', // learning in school, about programming, etc. 
 ]
--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,3 +1,10 @@
 export const WORDS = [
-  'texas',
+  'state', // States In america
+  'texas', // Used to live in texas
+  'potus', // president of the united states
+  'money', // $
+  'tests', // tests in school
+  'class', // classes in school
+  'study', // studying in school 
+  'learn', // learning in school, about programming, etc. 
 ]
